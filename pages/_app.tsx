e4fe04bda4a95fa48ae97d917f3f9408013964e0
Anders Aaen Springborg <anders.springborg@askcody.com>
2023-03-29 21:24:19 +0200

    add themeprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
+import { ThemeProvider } from "next-themes";
 import "../styles.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return(
+        <ThemeProvider attribute="class">
+          <Component {...pageProps} />
+        </ThemeProvider>)
+;
 }
