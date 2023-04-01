5c76afc2a899d6e651a4d8d5a833f169556b8aea
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-04-01 08:14:54 +0200

    updated headers

--- components/header.tsx
+++ components/header.tsx
@@ -1,30 +1,32 @@
 import Head from "next/head";
 
 const data = {
-    title: "Learn Roundnet",
+        title: "Learn Roundnet",
+        description: "Learn Roundnet is the one place for all your roundnet needs",
+        link: "https://learn-roundnet.vercel.app/"
 }
 
-const path_to_logo = "https://sbcc.io/img/logo-1200.png";
+const path_to_logo = "https://learn-roundnet.vercel.app/android-chrome-512x512.png";
 const Headers = () => {
     return (
         <Head>
             {/*!--Primary Meta Tags --*/}
             <title>{data.title}</title>
             <meta name="title" content={data.title}/>
-            <meta name="description" content={data.title + "is a Single Board Cluster Competition made for students by students"}/>
+            <meta name="description" content={data.description}/>
 
             {/*<!-- Open Graph / Facebook --> */}
             <meta property="og:type" content="website"/>
             <meta property="og:url" content="https://sbcc.io/"/>
             <meta property="og:title" content={data.title}/>
-            <meta property="og:description" content={data.title + "is a Single Board Cluster Competition made for students by students"}/>
+            <meta property="og:description" content={data.description}/>
             <meta property="og:image" content={path_to_logo}/>
 
             {/*<!-- Twitter -->*/}
             <meta property="twitter:card" content="summary_large_image"/>
-            <meta property="twitter:url" content="https://sbcc.io/"/>
+            <meta property="twitter:url" content={data.link}/>
             <meta property="twitter:title" content={data.title}/>
-            <meta property="twitter:description" content={data.title + "is a Single Board Cluster Competition made for students by students"}/>
+            <meta property="twitter:description" content={data.description}/>
             <meta property="twitter:image" content={path_to_logo}/>
             {/* favicon */}
             <link rel="icon" href="/favicon.ico"/>
