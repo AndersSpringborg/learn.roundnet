1a2068e56b2dc9e7a19f67409a56ba905e5b93b7
Anders Aaen Springborg <anders.springborg@askcody.com>
2023-04-07 14:44:42 +0200

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/react';
 import "../styles.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />  
     </ThemeProvider>
   );
 }
